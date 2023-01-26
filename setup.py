38c77a8a71466ad0cda0f4b63b52f9d5c1188ad2
Will <37912237+WilliamJudge94@users.noreply.github.com>
2023-01-26 11:16:18 -0600

    BUG: removing installs for easy integration

--- setup.py
+++ setup.py
@@ -29,24 +29,24 @@ setup(
     python_requires=">=3.7",
     # lower bounds based on Debian Stable versions where available
     install_requires=[
-        "matplotlib>=3.0.0, <4.0.0",
-        "numpy>=1.16.2, <2.0.0",
-        "pandas>=0.23.3, <2.0.0",
-        "Pillow>=5.4.1, <10.0.0",
-        "opencv-python>=3.2.0, <5.0.0",
-        "scipy>=1.3.0, <2.0.0",
-        'scikit-image>=0.14.2, !=0.17.1, <0.20',  # https://github.com/SeldonIO/alibi/issues/215
-        "scikit-learn>=0.20.2, <2.0.0",
-        "tensorflow>=2.2.0, !=2.6.0, !=2.6.1, <2.10.0",  # https://github.com/SeldonIO/alibi-detect/issues/375 and 387
-        "tensorflow_probability>=0.8.0, <0.18.0",
-        "transformers>=4.0.0, <5.0.0",
-        "dill>=0.3.0, <0.4.0",
-        "tqdm>=4.28.1, <5.0.0",
-        "requests>=2.21.0, <3.0.0",
-        "numba>=0.50.0, !=0.54.0, <0.56.0",  # Avoid 0.54 due to: https://github.com/SeldonIO/alibi/issues/466
-        "pydantic>=1.8.0, <2.0.0",
-        "toml>=0.10.1, <1.0.0",  # STC, see https://discuss.python.org/t/adopting-recommending-a-toml-parser/4068
-        "catalogue>=2.0.0, <3.0.0",
+#         "matplotlib>=3.0.0, <4.0.0",
+#         "numpy>=1.16.2, <2.0.0",
+#         "pandas>=0.23.3, <2.0.0",
+#         "Pillow>=5.4.1, <10.0.0",
+#         "opencv-python>=3.2.0, <5.0.0",
+#         "scipy>=1.3.0, <2.0.0",
+#         'scikit-image>=0.14.2, !=0.17.1, <0.20',  # https://github.com/SeldonIO/alibi/issues/215
+#         "scikit-learn>=0.20.2, <2.0.0",
+#         "tensorflow>=2.2.0, !=2.6.0, !=2.6.1, <2.10.0",  # https://github.com/SeldonIO/alibi-detect/issues/375 and 387
+#         "tensorflow_probability>=0.8.0, <0.18.0",
+#         "transformers>=4.0.0, <5.0.0",
+#         "dill>=0.3.0, <0.4.0",
+#         "tqdm>=4.28.1, <5.0.0",
+#         "requests>=2.21.0, <3.0.0",
+#         "numba>=0.50.0, !=0.54.0, <0.56.0",  # Avoid 0.54 due to: https://github.com/SeldonIO/alibi/issues/466
+#         "pydantic>=1.8.0, <2.0.0",
+#         "toml>=0.10.1, <1.0.0",  # STC, see https://discuss.python.org/t/adopting-recommending-a-toml-parser/4068
+#         "catalogue>=2.0.0, <3.0.0",
     ],
     extras_require=extras_require,
     test_suite="tests",
