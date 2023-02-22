dffc06cbd357691301e3b44c68023b7d6e63a6ea
William Judge <william@elementenergy.com>
2023-02-22 14:13:47 -0800

    FEAT: trying to reduce size

--- alibi_detect/__init__.py
+++ alibi_detect/__init__.py
@@ -1,4 +1,5 @@
 from . import ad, cd, models, od, utils, saving
 from .version import __version__  # noqa F401
 
-__all__ = ["ad", "cd", "models", "od", "utils", "saving"]
+#__all__ = ["ad", "cd", "models", "od", "utils", "saving"]
+__all__ = ["od", ]
