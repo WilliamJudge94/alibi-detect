0a42b63e768c905f023365ac1b27aa1f0ac26e42
William Judge <william@elementenergy.com>
2023-02-22 13:38:53 -0800

    UPDT: adding deps

--- setup.py
+++ setup.py
@@ -34,8 +34,9 @@ setup(
         "pandas>=0.23.3, <2.0.0",
         #"Pillow>=5.4.1, <10.0.0",
         #"opencv-python>=3.2.0, <5.0.0",
-        #"scipy>=1.3.0, <2.0.0",
-        # 'scikit-image>=0.14.2, !=0.17.1, <0.20',  # https://github.com/SeldonIO/alibi/issues/215
+        "scipy>=1.3.0, <2.0.0",
+        # https://github.com/SeldonIO/alibi/issues/215
+        'scikit-image>=0.14.2, !=0.17.1, <0.20',
         #"scikit-learn>=0.20.2, <2.0.0",
         # https://github.com/SeldonIO/alibi-detect/issues/375 and 387
         "tensorflow>=2.2.0, !=2.6.0, !=2.6.1, <2.10.0",
