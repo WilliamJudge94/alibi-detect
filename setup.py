7715bf12b1723327a21c65abc833c4c375335dce
William Judge <william@elementenergy.com>
2023-02-23 09:35:01 -0800

    UPDT: adding skimage back in

--- setup.py
+++ setup.py
@@ -36,7 +36,7 @@ setup(
         #"opencv-python-headless>=3.2.0, <5.0.0",
         "scipy>=1.3.0, <2.0.0",
         # https://github.com/SeldonIO/alibi/issues/215
-        #'scikit-image>=0.14.2, !=0.17.1, <0.20',
+        'scikit-image>=0.14.2, !=0.17.1, <0.20',
         "scikit-learn>=0.20.2, <2.0.0",
         # https://github.com/SeldonIO/alibi-detect/issues/375 and 387
         "tensorflow-cpu>=2.2.0, !=2.6.0, !=2.6.1, <2.10.0",
